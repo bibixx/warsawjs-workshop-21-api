fe96f7856eeaa25a9ca61403433ffa90d02ccfbf
Bartosz Legięć <bartosz@legiec.eu>
2018-06-21 19:55:30 +0200

    Add deleteFiles on startup

--- src/cronDeleteFiles.mjs
+++ src/cronDeleteFiles.mjs
@@ -59,6 +59,8 @@ export const cronDeleteFiles = () => {
   const CronJob = cron.CronJob;
   const cronPattern = process.env.CRON_PATTERN || "0 */10 * * * *";
 
+  deleteFiles();
+
   const job = new CronJob({
     cronTime: cronPattern,
     onTick: function() {
